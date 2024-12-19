8c440a5279042be027ecbd0aee648333de2495f1
MSE0612 <sskyfall0411@gmail.com>
2024-12-19 10:01:17 +0900

    first upload

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Android.Gradle.Manifest;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float health;
+    public float maxHealth;
+    public RuntimeAnimatorController[] animCon;
+    public Rigidbody2D target;
+
+    bool isLive = true;
+
+    Rigidbody2D rb;
+    Collider2D col;
+    Animator anim;
+    SpriteRenderer spriter;
+    WaitForFixedUpdate wait;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
+        spriter = GetComponent<SpriteRenderer>();
+        wait = new WaitForFixedUpdate();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit")) 
+            return;
+
+        Vector2 dirVec = target.position - rb.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + nextVec);
+        rb.velocity = Vector2.zero;
+    }
+
+    private void LateUpdate()
+    {
+        if (!isLive) return;
+        spriter.flipX = target.position.x < rb.position.x;
+    }
+
+    void OnEnable()
+    {
+        target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        isLive = true;
+        col.enabled = true;
+        rb.simulated = true;
+        spriter.sortingOrder = 2;
+        anim.SetBool("Dead", false);
+        health = maxHealth;
+    }
+
+    public void Init(SpawnData data)
+    {
+        anim.runtimeAnimatorController = animCon[data.spriteType];
+        speed = data.speed;
+        maxHealth = data.health;
+        health = data.health;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Bullet")) 
+            return;
+
+        health -= collision.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
+
+        if (health > 0) {
+            anim.SetTrigger("Hit");
+        }
+        else {
+            isLive = false;
+            col.enabled = false;
+            rb.simulated = false;
+            spriter.sortingOrder = 1;
+            anim.SetBool("Dead", true);
+        }
+    }
+
+    IEnumerator KnockBack()
+    {
+        yield return wait;
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        rb.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
+    }
+
+    void Dead()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rb;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        inputVec.x = Input.GetAxis("Horizontal");
+        inputVec.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextMove = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rb.velocity = inputVec * speed * Time.fixedDeltaTime;
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+}
